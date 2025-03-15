a8b4eb0c1eb4a6432d3a50c19faa13b7a4add599
Calderaly <calderaly16@gmail.com>
2025-03-15 10:02:17 +0700

    menambahkan class DataGeneric

--- MyGeneric.cs
+++ MyGeneric.cs
@@ -1,10 +1,27 @@
-﻿namespace MyGeneric
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace MyGeneric
 {
     public class HaloGeneric
     {
         public void SapaUser<X>(X nama)
         {
             Console.WriteLine($"Halo, selamat pagi {nama}");
+        }
+    }
+
+    public class DataGeneric<T>
+    {
+        public T? Data { get; set; }
+
+        public DataGeneric(T data)
+        {
+            this.Data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Nim kamu adalah: {Data}");
         }
     }
 }
